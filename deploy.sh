ab4a8b3fa47358d9e4452aaa28003d417125f5f1
Konstantin Tsepelev <strangeratthedoor@gmail.com>
2021-04-12 21:40:24 +0300

    chore: add cd

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+# Add version to package.json
+sed -i s/"\"version\":.*/\"version\": \"${TRAVIS_TAG}\","/ package.json
+
+# Put NPM access token to .npmrc for publishing
+echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
+
+# Publish new version of the app to NPM
+npm publish
